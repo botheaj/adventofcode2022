1d6d6d172ad8afda6d016ac90609b0f7272e4e35
AJ Bothe <aj.bothe@gmail.com>
2022-12-05 22:46:36 -0500

    day 6 placeholder

--- main.go
+++ main.go
@@ -7,19 +7,22 @@ import (
 )
 
 func main() {
-	fmt.Println("Day 1")
-	day.One("inputs/1.txt")
+	//fmt.Println("Day 1")
+	//day.One("inputs/1.txt")
+	//
+	//fmt.Println("Day 2")
+	//day.Two("inputs/2.txt")
+	//
+	//fmt.Println("Day 3")
+	//day.Three("inputs/3.txt")
+	//
+	//fmt.Println("Day 4")
+	//day.Four("inputs/4.txt")
+	//
+	//fmt.Println("Day 5")
+	//fmt.Println(day.Five("inputs/5.txt"))
+	//fmt.Println(day.FivePart2("inputs/5.txt"))
 
-	fmt.Println("Day 2")
-	day.Two("inputs/2.txt")
-
-	fmt.Println("Day 3")
-	day.Three("inputs/3.txt")
-
-	fmt.Println("Day 4")
-	day.Four("inputs/4.txt")
-
-	fmt.Println("Day 5")
-	fmt.Println(day.Five("inputs/5.txt"))
-	fmt.Println(day.FivePart2("inputs/5.txt"))
+	fmt.Println("Day 6")
+	day.Six("inputs/6.txt")
 }
